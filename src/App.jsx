5328df40c361f43f6d184b342eb338c022113895
b3ingsahil007 <chhipasahil163@gmail.com>
2025-04-21 13:25:15 +0530

    broker page

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import AdminDashboard from "./pages/admin-dashboard-pages/AdminDashboard";
 import AllProperties from "./pages/admin-dashboard-pages/AllProperties";
 import Profile from "./pages/customer-dashboard-pages/Profile";
 import PropertyDetails from "./pages/PropertyDetails";
+import Brokers from "./pages/admin-dashboard-pages/Brokers";
 
 const App = () => {
   const { authUser } = useAuthContext();
@@ -39,6 +40,7 @@ const App = () => {
         {/* Admin Dashboard */}
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/properties" element={<AllProperties />} />
+        <Route path="/admin/brokers" element={<Brokers />} />
       </Routes>
       <div>
         <Toaster />
